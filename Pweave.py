3b4db198bac0e693062515e7dddd77bfada4c1f8
Mark Edgington <edgimar@gmail.com>
2011-01-13 12:57:24 +0100

    - added GPLv3 header and license text - added README file with some basic details on installing/using Pweave

--- Pweave.py
+++ Pweave.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
-# Pweave, Literate programming tool for Python 
-# ============================================
+# Pweave -- A literate programming tool for python 
+#
+# Copyright (C) 2010, Matti Pastell <matti.pastell@helsinki.fi>
+# Copyright (C) 2011, Mark Edgington <edgimar@gmail.com>
 # 
-# :Author: Matti Pastell <matti.pastell@helsinki.fi>
-# :Website: http://mpastell.com
-# Version: 0.12
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 
 import sys
 import StringIO
